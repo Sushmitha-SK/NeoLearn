04f5130186a31c63859dbcba8b2c54a512bf415f
Sushmitha-SK <sushmitaudupi@gmail.com>
2025-07-03 19:20:25 +0530

    Designed Homepage

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import type { ReactNode } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 interface AppContextType {
 }
@@ -14,7 +16,38 @@ const defaultContextValue: AppContextType = {
 export const AppContext = createContext<AppContextType>(defaultContextValue);
 
 export const AppContextProvider = ({ children }: AppContextProviderProps) => {
+
+    const currency = import.meta.env.VITE_CURRENCY
+    const navigate = useNavigate()
+
+    const [allCourses, setAllCourses] = useState([])
+    const [isEducator, setIsEducator] = useState(true)
+
+    // Fetch all courses
+    const fetchAllCourses = async () => {
+        setAllCourses(dummyCourses)
+    }
+
+    // Function to calculate average rating of course
+    const calculateRating = (course) => {
+        if (course.courseRatings.length === 0) {
+            return 0;
+        }
+        let totalRating = 0
+        course.courseRatings.forEach(rating => {
+            totalRating += rating.rating
+        })
+        return totalRating / course.courseRatings.length
+    }
+
+    useEffect(() => {
+        fetchAllCourses()
+    }, [])
+
+
     const value: AppContextType = {
+        currency, allCourses, navigate, calculateRating,
+        isEducator,setIsEducator
     };
 
     return (

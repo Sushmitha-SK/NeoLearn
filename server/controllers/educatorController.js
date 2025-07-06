656adc4b66209514a7c0032522b231e6940a0e71
Sushmitha-SK <sushmitaudupi@gmail.com>
2025-07-06 18:26:17 +0530

    API for User, Course, CourseProgress and Purchase

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,6 +1,6 @@
 import { clerkClient } from '@clerk/express'
 import Course from '../models/Course.js'
-import Purchase from '../models/Purchase.js'
+import { Purchase } from '../models/Purchase.js'
 import { v2 as cloudinary } from 'cloudinary'
 
 //Update role to educator
--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
+import courseRouter from './routes/courseRoutes.js'
+import userRouter from './routes/userRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
 
@@ -22,6 +24,9 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send("API Working"))
 app.post('/', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 //Port
 const PORT = process.env.PORT || 5000

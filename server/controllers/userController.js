a12d60a1925a790090096c4b802e211ca3196410
Sushmitha S <94210498+Sushmitha-SK@users.noreply.github.com>
2025-07-15 10:18:11 +0530

    Updated userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import Course from "../models/Course.js"
 import { Purchase } from "../models/Purchase.js"
+import { CourseProgress } from "../models/CourseProgress.js";
 import Stripe from 'stripe'
 
 //Get User Data
